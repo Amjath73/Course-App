6596c4d1ffa1eea578ac5216a08e4721fffc5096
Amjath73 <amjathsanker4@gmail.com>
2024-06-12 12:52:41 +0530

    search and delete api added

--- app.js
+++ app.js
@@ -3,32 +3,58 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const { coursemodel } = require("./models/course")
 
-const app=express()
+const app = express()
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://amjath:itsArkingtime7@cluster0.n01k0zd.mongodb.net/courseDB?retryWrites=true&w=majority&appName=Cluster0")
 
-app.post("/add",(req,res)=>{
+app.post("/add", (req, res) => {
     let input = req.body
-    let course=new coursemodel(input)
+    let course = new coursemodel(input)
     console.log(course)
     course.save()
-    res.json({"status":"test"})
+    res.json({ "status": "test" })
 })
 
-app.get("/view",(req,res)=>{
+app.get("/view", (req, res) => {
     coursemodel.find().then(
-        (data)=>(
+        (data) => {
             res.json(data)
-        )
+    }
     ).catch(
-        (error)=>{
+        (error) => {
             res.json(error)
         }
     )
 })
 
-app.listen(8080,()=>{
+app.post("/search", (req, res) => {
+    let input = req.body
+    coursemodel.find(input).then(
+        (data) => { res.json(data) }
+    ).catch(
+        (error) => {
+            res.json(error)
+        }
+    )
+})
+
+app.post("/delete",(req,res)=>{
+    let input=req.body
+    coursemodel.findByIdAndDelete(input._id).then(
+        (response)=>{
+        res.json({"status":"success"})
+        }
+    ).catch(
+        (error)=>
+            {
+                res.json({"status":"error"})
+            }
+    )
+})
+
+
+app.listen(8081, () => {
     console.log("Server started")
 })
